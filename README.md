ca5bf0c60651b2089ef53566c11f6cf5811356dc
I_am_not_a_weeb <kubutek04@gmail.com>
2023-12-10 15:06:34 +0100

    rest

---
+++ README.md
@@ -0,0 +1,15 @@
+﻿## REST API do neo4j
+
+#### Do kompilacji potrzebne są conajmniej:
+ - Visual Studio 2022 
+ - CppRestSDK -  https://github.com/microsoft/cpprestsdk
+ 
+#### Działa:
+ 
+ - [x]  3. GET /employees?sort={}&filter={} po jednym do kazdego
+ - [x]  4. POST /employees -> json
+ - [x]  5. PUT /employees/:id -> json
+ - [ ]  6. DELETE /employees/:id
+ - [ ]  7. GET /employees/:id/subordinates
+ - [ ]  8.
+ - [ ]  9. GET /departments/:id/employees
--- REST api neo4j.cpp
+++ REST api neo4j.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <cpprest/http_listener.h>
 #include <cpprest/http_client.h>
-#include <boost/beast/core/detail/base64.hpp>
+//#include <boost/beast/core/detail/base64.hpp>
+
+#include <cpprest/asyncrt_utils.h>
 
 const utility::string_t authStr = L"bmVvNGo6dGVzdDEyMzQ=";
 const utility::string_t url = L"http://localhost:7474/db/neo4j/tx/commit";
@@ -62,6 +64,7 @@ void handleRequest(web::http::http_request request, std::function<void(web::json
 
 void handleGET(web::http::http_request request);
 void handlePOST(web::http::http_request request);
+void handlePUT(web::http::http_request request);
 
 int main()
 {
@@ -70,35 +73,43 @@ int main()
 
 	listener.support(web::http::methods::GET, std::bind(handleGET, std::placeholders::_1));
 	listener.support(web::http::methods::POST, std::bind(handlePOST, std::placeholders::_1));
-	
+	listener.support(web::http::methods::PUT, std::bind(handlePUT, std::placeholders::_1));
 
-   	web::http::http_request request(web::http::methods::POST);
+   	/*web::http::http_request request(web::http::methods::POST);*/
 
 	// bugi woogi
 	//web::json::object* kek = &web::json::value::parse(L"{\"kek1\":1,\"kek2\":-1}").as_object();
 	//web::json::object* kek = new web::json::object(web::json::value::parse(L"{\"kek1\":1,\"kek2\":-1}").as_object());
 
-	request.headers().add(L"Authorization", authStr);
+	/*request.headers().add(L"Authorization", authStr);
 	request.headers().add(L"Content-Type", L"application/json");
-	request.set_body(L"{\"statements\":[{\"statement\":\"MATCH (e:Employee) RETURN e\"}]}");
+	request.set_body(L"{\"statements\":[{\"statement\":\"MATCH (e:Employee) RETURN e\"}]}");*/
 
-	std::string toprint;
-	client->request(request).then([&](web::http::http_response response)
-	{
-		try 
-		{
-			std::cout << response.status_code() << std::endl;
-			response.extract_json().then([&](web::json::value json)
-				{
-					toprint = utility::conversions::to_utf8string(json.serialize());
-				}).wait();
-		}
-		catch (std::exception& e)
-		{
-			std::cout << e.what() << std::endl;
-		}
-	}).wait();
-	std::cout << toprint << std::endl;
+	//std::string toprint;
+	//try
+	//{
+	//	client->request(request).then([&](web::http::http_response response)
+	//		{
+	//			try
+	//			{
+	//				std::cout << response.status_code() << std::endl;
+	//				response.extract_json().then([&](web::json::value json)
+	//					{
+	//						toprint = utility::conversions::to_utf8string(json.serialize());
+	//					}).wait();
+	//			}
+	//			catch (std::exception& e)
+	//			{
+	//				std::cout << e.what() << std::endl;
+	//			}
+	//		}).wait();
+	//		std::cout << toprint << std::endl;
+	//}
+	//catch(std::exception& e)
+	//{
+	//			std::cout << e.what() << std::endl;
+	//}
+	
 
 
 	try
@@ -113,8 +124,113 @@ int main()
 		std::wcout << e.what() << std::endl;
 	}
 
-
 	delete client;
+}
+
+void handleDELETE(web::http::http_request request)
+{
+		std::vector<utility::string_t>path = web::http::uri::split_path(web::http::uri::decode(request.relative_uri().path()));
+
+	web::json::value answer = web::json::value::object();
+
+	if (path.empty())
+	{
+		request.reply(web::http::status_codes::BadRequest, L"Bad request");
+	}
+	else if (path[0] == L"employees")
+	{
+		if (path.size() == 1)
+		{
+			request.reply(web::http::status_codes::BadRequest, L"No ID");
+		}
+		else if (path.size() > 1 && isNumber(path[1]))
+		{
+			try
+			{
+				utility::stringstream_t queryStr;
+				queryStr << L"{\"statements\":[{\"statement\":\"MATCH (e:Employee) where e.id=";
+				queryStr << L"\\\"";
+				queryStr << path[1];
+				queryStr << L"\\\"";
+				queryStr << L" DETACH DELETE e \"}]}";
+
+				web::http::http_request requestToNEO4j(web::http::methods::POST);
+
+				requestToNEO4j.headers().add(L"Authorization", authStr);
+				requestToNEO4j.headers().add(L"Content-Type", L"application/json");
+
+				requestToNEO4j.set_body(web::json::value::parse(queryStr.str()));
+
+				client->request(requestToNEO4j).then([&](const web::http::http_response& responseFromNEO4j)
+					{
+						request.reply(responseFromNEO4j.status_code(), responseFromNEO4j.extract_json().get());
+					});
+			}
+			catch (std::exception& e)
+			{
+				request.reply(web::http::status_codes::Conflict, e.what());
+			}
+		}
+	}
+}
+
+void handlePUT(web::http::http_request request)
+{
+	std::vector<utility::string_t>path = web::http::uri::split_path(web::http::uri::decode(request.relative_uri().path()));
+
+	web::json::value answer = web::json::value::object();
+
+	if(path.empty())
+	{
+		request.reply(web::http::status_codes::BadRequest, L"Bad request");
+	}
+	else if (path[0] == L"employees")
+	{
+		if (path.size() == 1)
+		{  
+			request.reply(web::http::status_codes::BadRequest, L"No ID");
+		}
+		else if (path.size() > 1 && isNumber(path[1]))
+		{
+			try
+			{
+				web::json::value json = request.extract_json().get();
+
+				utility::stringstream_t queryStr;
+				queryStr << L"{\"statements\":[{\"statement\":\"MATCH (e:Employee) where e.id=";
+				queryStr << L"\\\"";
+				queryStr << path[1];
+				queryStr << L"\\\"";
+
+				for(const auto& i : json.as_object())
+				{
+					queryStr << L" SET e.";
+					queryStr << i.first;
+					queryStr << L"=\\\"";
+					queryStr << i.second.as_string();
+					queryStr << L"\\\"";
+				}
+				queryStr << L"\\\" return e \"}]}";
+
+				web::http::http_request requestToNEO4j(web::http::methods::POST);
+
+				requestToNEO4j.headers().add(L"Authorization", authStr);
+				requestToNEO4j.headers().add(L"Content-Type", L"application/json");
+
+				requestToNEO4j.set_body(web::json::value::parse(queryStr.str()));
+
+				client->request(requestToNEO4j).then([&](const web::http::http_response& responseFromNEO4j)
+					{
+						request.reply(responseFromNEO4j.status_code(),responseFromNEO4j.extract_json().get());
+					});
+			}
+			catch (std::exception& e)
+			{
+				request.reply(web::http::status_codes::Conflict, e.what());
+			}
+
+		}
+	}
 }
 
 void handlePOST(web::http::http_request request)
@@ -131,9 +247,73 @@ void handlePOST(web::http::http_request request)
 	{
 		if (path.size() == 1)
 		{
-			web::json::object body = request.extract_json().get().as_object();
+			try {
+				web::json::object body = request.extract_json().get().as_object();
+				if (body[L"name"].as_string() != L"null" &&
+					body[L"department"].as_string() != L"null")
+				{
+					web::http::http_request requestToNEO4j(web::http::methods::POST);
 
-			true;
+					requestToNEO4j.headers().add(L"Authorization", authStr);
+					requestToNEO4j.headers().add(L"Content-Type", L"application/json");
+
+					utility::stringstream_t queryStr;
+					queryStr << L"{\"statements\":[{\"statement\":\"MATCH (e:Employee) where e.name=";
+					queryStr << L"\\\"";
+					queryStr << body[L"name"].as_string();
+					queryStr << L"\\\" return e \"}]}";
+
+					requestToNEO4j.set_body(web::json::value::parse(queryStr.str()));
+
+					client->request(requestToNEO4j).then([&](web::http::http_response responseFromNEO4j)
+						{
+							try
+							{
+								std::cout << responseFromNEO4j.status_code() << std::endl;
+								web::json::value json = responseFromNEO4j.extract_json().get();
+
+								if (json[L"results"].as_array().at(0).as_object()[L"data"].as_array().size() != 0)
+								{
+									request.reply(web::http::status_codes::BadRequest, L"Employee already exists");
+								}
+								else
+								{
+									utility::stringstream_t queryStr;
+									queryStr << L"{\"statements\":[{\"statement\":\"CREATE (e:Employee {name:\\\"";
+									queryStr << body[L"name"].as_string();
+									queryStr << L"\\\", department:\\\"";
+									queryStr << body[L"department"].as_string();
+									queryStr << L"\\\"}) return e \"}]}";
+
+									requestToNEO4j.set_body(web::json::value::parse(queryStr.str()));
+
+									client->request(requestToNEO4j).then([&](web::http::http_response responseFromNEO4j)
+										{
+											try
+											{
+												std::cout << responseFromNEO4j.status_code() << std::endl;
+												web::json::value json = responseFromNEO4j.extract_json().get();
+
+												request.reply(web::http::status_codes::OK, json);
+											}
+											catch (std::exception& e)
+											{
+												std::cout << e.what() << std::endl;
+											}
+										}).wait();
+								}	
+							}
+							catch (std::exception& e)
+							{
+								std::cout << e.what() << std::endl;
+							}
+						}).wait();
+				}
+			}
+			catch (std::exception& e)
+			{
+				request.reply(web::http::status_codes::BadRequest, e.what());
+			}
 		}
 	}
 }
@@ -163,11 +343,11 @@ void handleGET(web::http::http_request request)
 
 				for (auto const& query : queries)			// iteracja po query
 				{
-					if (query.first == L"filter")								// jezeli jest filter, koniecznie obiekt
+					if (query.first == L"filter")								// je¿eli jest filter, koniecznie obiekt
 					{
 						filterQuery = new web::json::object(web::json::value::parse(query.second).as_object());
 					}
-					else if (query.first == L"sort")							// jezeli jest sort, koniecznie obiekt
+					else if (query.first == L"sort")							// je¿eli jest sort, koniecznie obiekt
 					{
 						sortQuery = new web::json::object(web::json::value::parse(query.second).as_object());
 					}
